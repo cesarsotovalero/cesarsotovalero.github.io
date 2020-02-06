7f1a1d2f7daf4fd026165d81902b616288e70cde
cesarsotovalero <cesarsotovalero@gmail.com>
2020-02-06 14:17:52 +0100

    change logo

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -177,7 +177,6 @@
     triggerTransitionEnd: function triggerTransitionEnd(element) {
       $(element).trigger(TRANSITION_END);
     },
-    // TODO: Remove in v5
     supportsTransitionEnd: function supportsTransitionEnd() {
       return Boolean(TRANSITION_END);
     },
@@ -5009,7 +5008,7 @@
       }
     } // ----------------------------------------------------------------------
     // the following methods are used to handle overflowing modals
-    // todo (fat): these should probably be refactored out of modal.js
+    //
     // ----------------------------------------------------------------------
     ;
 
@@ -6315,7 +6314,6 @@
           var targetBCR = target.getBoundingClientRect();
 
           if (targetBCR.width || targetBCR.height) {
-            // TODO (fat): remove sketch reliance on jQuery position/offset
             return [$(target)[offsetMethod]().top + offsetBase, targetSelector];
           }
         }
