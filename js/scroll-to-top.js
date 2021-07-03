dec9d5eca84ef8660d194563775a5ec7ec38ccd6
cesarsotovalero <cesarsotovalero@users.noreply.github.com>
2021-07-03 15:45:47 +0000

    deploy: f7168d9a97bcaa66d7cb0b15bd4824642ef7ed05

--- js/scroll-to-top.js
+++
@@ -1,8 +0,0 @@
-function scrollToTop() {
-   var position =
-      document.body.scrollTop || document.documentElement.scrollTop;
-   if (position) {
-      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-      scrollAnimation = setTimeout("scrollToTop()", 30);
-   } else clearTimeout(scrollAnimation);
-}
