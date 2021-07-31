cfa98ace6a00a2a5c832a0f3ec8d4584ae0dbb5c
César Soto Valero <cesarsotovalero@gmail.com>
2021-07-31 09:20:32 +0200

    Add ssl file

--- js/navbar-hider.js
+++ js/navbar-hider.js
@@ -2,15 +2,15 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
    var currentScrollPos = window.pageYOffset;
    var elems = document.getElementsByClassName('navbar-fixed-top');
-   if (prevScrollpos > currentScrollPos) {
+   if (prevScrollpos > currentScrollPos ) {
       for (var i = 0; i < elems.length; i++) {
-         elems[i].style.transition = "top 0.5s ease-in-out";
-         elems[i].style.top = "0";
+         elems[i].style.transition = "top 0.4s ease-in-out";
+         elems[i].style.top = "0px";
       }
    } else {
       for (var i = 0; i < elems.length; i++) {
-         elems[i].style.transition = "top 1.5s ease-in-out";
-         elems[i].style.top = "-70px";
+         elems[i].style.transition = "top 0.4s ease-in-out";
+         elems[i].style.top = "-130px";
       }
    }
    prevScrollpos = currentScrollPos;
