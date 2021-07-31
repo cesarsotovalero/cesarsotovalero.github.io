5ab16b23b8b1dafa3cb08184ea654a171662e7ea
César Soto Valero <cesarsotovalero@gmail.com>
2021-07-31 10:51:19 +0200

    fix navbar hider

--- js/navbar-hider.js
+++ js/navbar-hider.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
    var elems = document.getElementsByClassName('navbar-fixed-top');
    if (prevScrollpos > currentScrollPos ) {
       for (var i = 0; i < elems.length; i++) {
-         elems[i].style.transition = "top 0.4s ease-in-out";
+         elems[i].style.transition = "top 0.2s ease-in-out";
          elems[i].style.top = "0px";
       }
    } else {
