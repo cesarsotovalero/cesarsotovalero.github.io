f8d150229e94a9e8ec61f5f2d2ace91324dc79d3
César Soto Valero <cesarsotovalero@gmail.com>
2025-06-06 22:37:00 +0200

    Update Gemfile.lock for new platform support and fix script execution syntax

--- run_jekyll.sh
+++ run_jekyll.sh
@@ -1,3 +1,2 @@
-python3 scripts/tag_generator.py \
-JEKYLL_ENV=production
+python3 scripts/tag_generator.py; JEKYLL_ENV=production
 RUBYOPT='-W0' bundle exec jekyll serve --watch --incremental future
